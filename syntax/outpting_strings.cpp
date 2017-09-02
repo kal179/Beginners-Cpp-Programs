605a5adda2b72f1b9d6eb5577c9ac90a905650fe
Kalpak Take <kalpaktake@gmail.com>
2017-09-02 15:12:10 +0530

    Update outpting_strings.cpp

--- syntax/outpting_strings.cpp
+++ syntax/outpting_strings.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(void){
 	// cout: used to output strings
 	// endl: indicator for end of line
+	// << : is insertion operator used to insert strings
 	cout << "Hey! I'm an array of characters, a string!" << endl;
 	return 0;
 }
