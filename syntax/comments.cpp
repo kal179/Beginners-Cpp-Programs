eaa84528d4744a605529292c6eb64ad9d9b34286
Kalpak Take <kalpaktake@gmail.com>
2017-09-02 14:22:58 +0530

    Create comments.cpp

---
+++ syntax/comments.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	// This is a single-line comment 
+	cout << "Comments: are useful for writing notes to programmer,\nComments are ignored by compiler,\nThey are useful to comment part of code to avoid its execution!" << endl;
+	/*
+		This is a multi-line comment
+		Commenting your source is really useful and good programming habit
+	*/
+	return 0;
+}
