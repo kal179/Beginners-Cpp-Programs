209acb16128165b9d871de361ccd89c19d2f0d7d
Kalpak Take <kalpaktake@gmail.com>
2017-09-02 14:17:25 +0530

    Create newlines_and_special_chars.cpp

---
+++ syntax/newlines_and_special_chars.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	// \n is a newline character
+	// whenever, wherever used puts a newline on console!
+	// some other useful characters,
+	// \b for backspace
+	// \t for tab 
+	cout << "These \nAre \nNewlines! \n";
+	return 0;
+}
