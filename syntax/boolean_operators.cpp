b86f939e1ce7a137e7487faf09b458486461b601
Kalpak Take <kalpaktake@gmail.com>
2017-09-02 20:39:50 +0530

    boolean operators in c++

---
+++ syntax/boolean_operators.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+// Boolean operators
+// Used in if, else if statements to compare 
+// one or more conditions in single if, else if statement 
+// Operators &&, ||, !
+// or 
+// and, or, not
+// used like `condition and condition`
+// and: if conditions on both sides of and 
+// evaluates to true then only the code in block is executed
+// or: if condition on either side of or  
+// evaluates to true then the code in block is executed
+// used like `not condition`
+// not: if condition evaluates to true then not condition is false and vice versa
+// not's, and's and or's can be used together and also form a one big condition/expression
+//
+// Boolean operator precendence(same as arithmetic operator precendence)
+// not is executed first, then and, then or 
+// parentheses is used to give priority(same as in arithmetic precendence)
+
+int main () {
+	cout << "Refer here: https://github.com/AsciiKay/Beginners-Cpp-Programs/blob/master/syntax/control_flow.cpp" << endl;	
+	return 0;
+}
