3d1dca64679c622d6a7b26c53a509fa2e35c62da
Kalpak Take <kalpaktake@gmail.com>
2017-09-02 14:08:14 +0530

    Create outpting_strings.cpp

---
+++ syntax/outpting_strings.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+	// cout: used to output strings
+	// endl: indicator for end of line
+	cout << "Hey! I'm an array of characters, a string!" << endl;
+	return 0;
+}
