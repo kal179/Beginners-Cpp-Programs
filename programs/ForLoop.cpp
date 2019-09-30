b27eb4c5aec6513f8bf9c6682593a2dc711a2165
Geethmaka Dissanayake <geethmaka@gmail.com>
2019-10-01 01:54:44 +0530

    ForLoopExample

---
+++ programs/ForLoop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+   for(int i=1; i<=6; i++){
+      /* This statement would be executed
+       * repeatedly until the condition
+       * i<=6 returns false.
+       */
+      cout<<"Value of variable i is: "<<i<<endl;
+   }
+   return 0;
+}
